82bc66a8e5e0bba155b737d9434c28bd953c8aad
Hajagos Martin <hajagos.martin@gmail.com>
2018-04-10 16:45:02 +0200

    Add PlayerMovement.cs script

---
+++ Survival Shooter/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 6f;
+
+    Vector3 movement;
+    Animator anim;
+    Rigidbody playerRigidbody;
+    int floorMask;
+    float camRayLength = 100f;
+
+    //Automatically called on start by unity
+    private void Awake()
+    {
+        floorMask = LayerMask.GetMask("Floor");
+        anim = GetComponent<Animator>();
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    /*
+     * Automatically called by unity, every time on physics component change.
+     * Applies to every physics character that has RigidBody attached.
+     */
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Move(horizontal, vertical);
+        Turning();
+        Animating(horizontal, vertical);
+    }
+   
+    void Move(float horizontal, float vertical)
+    {
+        movement.Set(horizontal, 0f, vertical);
+        movement = movement.normalized * speed * Time.deltaTime;
+
+        //Current position plus the input
+        playerRigidbody.MovePosition(transform.position + movement);
+    }
+
+    void Turning()
+    {
+        Ray camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit floorHit;
+
+        if (Physics.Raycast(camRay, out floorHit, camRayLength, floorMask))
+        {
+            Vector3 playerToMouse = floorHit.point - transform.position;
+            playerToMouse.y = 0f;
+
+            Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
+            playerRigidbody.MoveRotation(newRotation);
+        }
+    }
+
+    void Animating(float horizontal, float vertical)
+    {
+        bool walking = horizontal != 0f || vertical != 0f;
+
+        //Set value of 'walking' variable to the boolean condition 'IsWalking' (created in unity, animation) 
+        anim.SetBool("IsWalking", walking);
+    }
+}
