105a3d989567138dac90feb70047d9416a8b0974
Hajagos Martin <hajagos.martin@gmail.com>
2018-05-17 22:03:03 +0200

    Place shootable enemy on the scene for testing #7

--- Survival Shooter/Assets/Scripts/Player/PlayerHealth.cs
+++ Survival Shooter/Assets/Scripts/Player/PlayerHealth.cs
@@ -10,14 +10,14 @@ public class PlayerHealth : MonoBehaviour
     public int currentHealth;
     public Slider healthSlider;
     public Image damageImage;
-    public AudioClip deathClip;
+    //public AudioClip deathClip;
     public float flashSpeed = 5f;
     public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
 
     //Modify
 
     Animator anim;
-    AudioSource playerAudio;
+    //AudioSource playerAudio;
     PlayerMovement playerMovement;
     PlayerShooting playerShooting;
     bool isDead;
@@ -27,7 +27,7 @@ public class PlayerHealth : MonoBehaviour
     void Awake ()
     {
         anim = GetComponent <Animator> ();
-        playerAudio = GetComponent <AudioSource> ();
+        //playerAudio = GetComponent <AudioSource> ();
         playerMovement = GetComponent <PlayerMovement> ();
         playerShooting = GetComponentInChildren <PlayerShooting> ();
         currentHealth = startingHealth;
@@ -56,7 +56,7 @@ public class PlayerHealth : MonoBehaviour
 
         healthSlider.value = currentHealth;
 
-        playerAudio.Play ();
+        //playerAudio.Play ();
 
         if(currentHealth <= 0 && !isDead)
         {
@@ -73,8 +73,8 @@ public class PlayerHealth : MonoBehaviour
 
         anim.SetTrigger ("Die");
 
-        playerAudio.clip = deathClip;
-        playerAudio.Play ();
+        // playerAudio.clip = deathClip;
+        // playerAudio.Play ();
 
         playerMovement.enabled = false;
         playerShooting.enabled = false;
