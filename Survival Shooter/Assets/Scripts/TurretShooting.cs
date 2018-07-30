16bb35e82d6604588c685a60d2d9598413bb3a4c
Hajagos Martin <hajagos.martin@gmail.com>
2018-07-30 21:38:41 +0200

    Add muzzleflash to TurretShooting script and play when shooting is active #15

--- Survival Shooter/Assets/Scripts/TurretShooting.cs
+++ Survival Shooter/Assets/Scripts/TurretShooting.cs
@@ -7,43 +7,64 @@ public class TurretShooting : MonoBehaviour {
 	public int damagePerShot = 20;
     public float timeBetweenShots = 0.15f;
     public float range = 0.001f;
+	public Light gunLight;	
+	public ParticleSystem muzzleFlash;
 
 	float timer;
     Ray shootRay = new Ray();
 	LineRenderer gunLine;
     int shootableMask;
 
+	float effectsDisplayTime = 0.2f;
+
 	void Awake () {
 		shootableMask = LayerMask.GetMask ("Shootable");
         gunLine = GetComponent <LineRenderer> ();
-
         // gunAudio = GetComponent<AudioSource> ();
-        // gunLight = GetComponent<Light> ();
-
 	}
+
 
 	void Update () {
-		//Debug.Log("Turret - isEnemyNearby: " + isEnemyNearby());
-	}
-
-	void FixedUpdate() {
-
 		timer += Time.deltaTime;
 		if(timer >= timeBetweenShots && Time.timeScale != 0) {
 			attackNearbyEnemies();
 		}
+
+		if(timer >= timeBetweenShots * effectsDisplayTime)
+        {
+            DisableEffects();
+        }
 	}
+	public void DisableEffects () {
+        muzzleFlash.Stop();
+		gunLight.enabled = false;
+    }
 
 	Transform[] getTransformsOfEnemies(List<EnemyHealth> enemies) {
 		Transform[] enemyTransforms = new Transform[enemies.Count];
-
 		for (int i = 0; i < enemies.Count; i++) {
 			enemyTransforms[i] = enemies[i].transform;
 		}
 
 		return enemyTransforms;
 	}
-	
+
+	void attackNearbyEnemies() {	
+		timer = 0f;
+		List<EnemyHealth> enemiesInRange = this.enemiesInRange();
+		EnemyHealth closestEnemy = getClosestEnemy(enemiesInRange);
+		if (closestEnemy != null && closestEnemy.currentHealth > 0) {
+			Vector3 enemyPosition = closestEnemy.transform.position;
+			transform.parent.LookAt(enemyPosition);
+
+			gunLight.enabled = true;
+			muzzleFlash.gameObject.SetActive(true);
+       		muzzleFlash.Simulate(1);
+        	muzzleFlash.Play();
+			
+			closestEnemy.TakeDamage(damagePerShot, new Vector3(enemyPosition.x, 1f, enemyPosition.z));
+		}
+	}
 
 	EnemyHealth getClosestEnemy(List<EnemyHealth> enemies) {
 		float minDist = Mathf.Infinity;
@@ -82,33 +103,4 @@ public class TurretShooting : MonoBehaviour {
 		return enemies;
 	}
 
-	void attackNearbyEnemies() {
-		
-		// bool enemyInRange = false;
-		// int enemyCount = 0;
-		//Debug.Log("hitcolliders count : " + hitColliders.Length);
-		
-		List<EnemyHealth> enemiesInRange = this.enemiesInRange();
-		EnemyHealth closestEnemy = getClosestEnemy(enemiesInRange);
-		if (closestEnemy != null) {
-			Vector3 enemyPosition = closestEnemy.transform.position;
-			closestEnemy.TakeDamage(damagePerShot, new Vector3(enemyPosition.x, 0.5f, enemyPosition.z));
-		}
-		// for (int i = 0; i < hitColliders.Length; i++) {
-		// 	Collider c = hitColliders[i];
-		// 	EnemyHealth enemy = c.gameObject.transform.GetComponentInParent<EnemyHealth>();
-			
-		// 	if (enemy != null) {
-		// 		Debug.Log("nearby enemy: " + enemy);
-		// 		enemyInRange = true;
-		// 		enemyCount++;
-
-		// 		enemy.TakeDamage(damagePerShot, enemy.transform.position);
-		// 	}
-		// }
-
-		// if (enemyCount > 0) {
-		// 	Debug.Log("Enemy count: " + enemyCount);
-		// }
-	}
 }
