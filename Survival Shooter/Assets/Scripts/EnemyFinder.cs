f66a4ecb0efce09a98ade7249feddf9e80c509e4
Hajagos Martin <hajagos.martin@gmail.com>
2018-07-31 00:07:36 +0200

    Move near enemy detection to EnemyFinder class #15

---
+++ Survival Shooter/Assets/Scripts/EnemyFinder.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System.Linq;
+
+public class EnemyFinder : MonoBehaviour {
+
+	public static List<EnemyHealth> getEnemiesInRange(Vector3 origin, float range) {
+		List<EnemyHealth> enemies = new List<EnemyHealth>();
+
+		Collider[] hitColliders = Physics.OverlapSphere(origin, range);
+		for (int i = 0; i < hitColliders.Length; i++) {
+			Collider c = hitColliders[i];
+			EnemyHealth enemy = c.gameObject.transform.GetComponentInParent<EnemyHealth>();	
+			if (enemy != null) {
+				enemies.Add(enemy);
+			}
+		}
+		return enemies;
+	}
+
+	public static List<EnemyHealth> getClosestEnemies(Vector3 origin, float range, int enemyCount = 1) {
+		List<EnemyHealth> enemies = getEnemiesInRange(origin, range);
+		enemies.Sort(new DistanceComparer(origin));
+		if (enemies.Count >= enemyCount) {
+			return enemies.GetRange(0, enemyCount);
+		} else {
+			return enemies;
+		}
+	}
+
+	public class DistanceComparer : IComparer<EnemyHealth> {
+		private Vector3 origin;
+
+		public DistanceComparer(Vector3 origin) {
+			this.origin = origin;
+		}
+        public int Compare(EnemyHealth e1, EnemyHealth e2)
+        {
+			return (int) (Vector3.Distance(e1.transform.position, this.origin) - Vector3.Distance(e2.transform.position, this.origin));
+        }
+    }
+
+}
--- Survival Shooter/Assets/Scripts/TurretShooting.cs
+++ Survival Shooter/Assets/Scripts/TurretShooting.cs
@@ -17,90 +17,44 @@ public class TurretShooting : MonoBehaviour {
 
 	float effectsDisplayTime = 0.2f;
 
-	void Awake () {
+	void Awake() {
 		shootableMask = LayerMask.GetMask ("Shootable");
         gunLine = GetComponent <LineRenderer> ();
         // gunAudio = GetComponent<AudioSource> ();
 	}
 
 
-	void Update () {
+	void Update() {
 		timer += Time.deltaTime;
-		if(timer >= timeBetweenShots && Time.timeScale != 0) {
-			attackNearbyEnemies();
+		if (timer >= timeBetweenShots && Time.timeScale != 0) {
+			attackClosestEnemy();
 		}
 
-		if(timer >= timeBetweenShots * effectsDisplayTime)
-        {
+		if (timer >= timeBetweenShots * effectsDisplayTime) {
             DisableEffects();
         }
 	}
-	public void DisableEffects () {
+	public void DisableEffects() {
         muzzleFlash.Stop();
 		gunLight.enabled = false;
     }
 
-	Transform[] getTransformsOfEnemies(List<EnemyHealth> enemies) {
-		Transform[] enemyTransforms = new Transform[enemies.Count];
-		for (int i = 0; i < enemies.Count; i++) {
-			enemyTransforms[i] = enemies[i].transform;
-		}
-
-		return enemyTransforms;
-	}
-
-	void attackNearbyEnemies() {	
+	void attackClosestEnemy() {	
 		timer = 0f;
-		List<EnemyHealth> enemiesInRange = this.enemiesInRange();
-		EnemyHealth closestEnemy = getClosestEnemy(enemiesInRange);
-		if (closestEnemy != null && closestEnemy.currentHealth > 0) {
-			Vector3 enemyPosition = closestEnemy.transform.position;
-			transform.parent.LookAt(enemyPosition);
+		List<EnemyHealth> enemiesInRange = EnemyFinder.getClosestEnemies(this.transform.position, this.range, 5);
+		if (enemiesInRange.Count > 0) {
+			EnemyHealth closestEnemy = enemiesInRange[0];
+			if (closestEnemy != null && closestEnemy.currentHealth > 0) {
+				Vector3 enemyPosition = closestEnemy.transform.position;
+				transform.parent.LookAt(enemyPosition);
 
-			gunLight.enabled = true;
-			muzzleFlash.gameObject.SetActive(true);
-       		muzzleFlash.Simulate(1);
-        	muzzleFlash.Play();
-			
-			closestEnemy.TakeDamage(damagePerShot, new Vector3(enemyPosition.x, 1f, enemyPosition.z));
-		}
+				gunLight.enabled = true;
+				muzzleFlash.gameObject.SetActive(true);
+				muzzleFlash.Simulate(1);
+				muzzleFlash.Play();
+				
+				closestEnemy.TakeDamage(damagePerShot, new Vector3(enemyPosition.x, 1f, enemyPosition.z));
+			}	
+		}	
 	}
-
-	EnemyHealth getClosestEnemy(List<EnemyHealth> enemies) {
-		float minDist = Mathf.Infinity;
-		Vector3 currentPos = transform.position;
-		int indexOfNearestEnemy = 0;
-		int currentEnemyIndex = 0;
-
-		Transform[] enemyTransforms = getTransformsOfEnemies(enemies);
-		foreach (Transform t in enemyTransforms) {
-			float dist = Vector3.Distance(t.position, currentPos);
-			if (dist < minDist) {
-				minDist = dist;
-				indexOfNearestEnemy = currentEnemyIndex;
-			}
-			currentEnemyIndex++;
-		}
-
-		if (enemies.Count - 1 >= indexOfNearestEnemy) {
-			return enemies[indexOfNearestEnemy];
-		} else {
-			return null;
-		}
-	}
-
-	List<EnemyHealth> enemiesInRange() {
-		List<EnemyHealth> enemies = new List<EnemyHealth>();
-
-		Collider[] hitColliders = Physics.OverlapSphere(transform.position, range);
-		for (int i = 0; i < hitColliders.Length; i++) {
-			Collider c = hitColliders[i];
-			EnemyHealth enemy = c.gameObject.transform.GetComponentInParent<EnemyHealth>();	
-			if (enemy != null) {
-				enemies.Add(enemy);
-			}
-		}
-		return enemies;
-	}
-
 }
